f81f3b14fd2a8a1671d066c1f6333dceab3e405a
bobsel20d <bobsel20d@rambler.ru>
2023-10-08 16:47:26 +0500

    a little style changes

--- src/components/TaskForm.vue
+++ src/components/TaskForm.vue
@@ -8,7 +8,11 @@
                 placeholder="Введите название задачи"
                 @keypress.enter="createTask"
                 v-model="task.title">
-            <button class="btn" @click="createTask">Добавить</button>
+            <button class="btn btn_icon" @click="createTask">
+                
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"></path></svg>
+            
+            </button>
           </div>
         </div>
 </template>
